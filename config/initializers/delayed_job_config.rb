e8f4f5c92768509b2b54ca4c2993184fb67eb70f
Duke <dukekhaos@gmail.com>
2016-12-31 11:51:26 -0200

    remove raise_signal_exceptions = :term

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -3,5 +3,4 @@ Delayed::Worker.sleep_delay = 5
 Delayed::Worker.max_attempts = 50
 Delayed::Worker.default_queue_name = 'default'
 Delayed::Worker.delay_jobs = !Rails.env.test?
-Delayed::Worker.raise_signal_exceptions = :term
 Delayed::Worker.logger = Logger.new(STDOUT)
