b3843ae3db14940da1eea7ae86f4f73a4aa49df2
Duke <dukekhaos@gmail.com>
2016-12-31 09:28:31 -0200

    add configuration to delayed_job

---
+++ config/initializers/delayed_job_config.rb
@@ -0,0 +1,7 @@
+Delayed::Worker.destroy_failed_jobs = false
+Delayed::Worker.sleep_delay = 5
+Delayed::Worker.max_attempts = 50
+Delayed::Worker.default_queue_name = 'default'
+Delayed::Worker.delay_jobs = !Rails.env.test?
+Delayed::Worker.raise_signal_exceptions = :term
+Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'))
